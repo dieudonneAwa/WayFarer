759ea5b8c0350e81bc3e555a3589c58ca11f2620
awadieudone <awadieudone@gmail.com>
2019-07-02 10:25:56 +0100

    [chore] :server: update express server

--- src/server.js
+++ src/server.js
@@ -4,9 +4,7 @@ const app = express();
 
 app.get('/', (req, res) => res.send({ message: 'Welcome to my API' }));
 
-module.exports = () => {
-  return 'My Server';
-};
-
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server running on port ${port}...`));
+
+export default app;
--- test/server.test.js
+++
@@ -1,9 +0,0 @@
-import { assert } from 'chai';
-import server from '../src/server';
-
-describe('Server', () => {
-  it('Server: It should return My Server', (done) => {
-    assert.equal(server(), 'My Server');
-    done();
-  });
-});

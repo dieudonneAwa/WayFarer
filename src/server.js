50faca2c59fb8b4094b22aeb42b43390ac7b6806
awadieudone <awadieudone@gmail.com>
2019-07-04 16:51:32 +0100

    [chore] : server : update server, package.json, package-lock.json and userModel.js to support signup endpoint

--- src/server.js
+++ src/server.js
@@ -1,8 +1,16 @@
-import express from 'express'; 
+import express from 'express';
+import bodyParser from 'body-parser';
+import Signup from '../auth/signup';
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/json' }));
+
 app.get('/', (req, res) => res.send({ message: 'Welcome to my API' }));
+app.post('/auth/signup', Signup.signUp);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server running on port ${port}...`));

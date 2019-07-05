4f89406b92de97eaf48d0171338052c4715cccce
awadieudone <awadieudone@gmail.com>
2019-07-05 15:31:37 +0100

    [feature] : trip : create a trip

--- models/tripModel.js
+++ models/tripModel.js
@@ -1,73 +1,38 @@
-import uuid from 'uuid';
+/* eslint-disable class-methods-use-this */
+import db from '../db';
 
-class Trip {
-  /**
-   * class constructor
-   * @param data
-   */
-  constructor() {
-    this.trips = [];
+export default class Trip {
+  constructor(trip) {
+    if (trip && trip.id) {
+      this.id = trip.id;
+    }
+    this.bus_id = trip && trip.bus_id ? trip.bus_id : 0;
+    this.origin = trip && trip.origin ? trip.origin : 0;
+    this.destination = trip && trip.destination ? trip.destination : 0;
+    this.trip_date = trip && trip.trip_date ? trip.trip_date : 0;
+    this.fare = trip && trip.fare ? trip.fare : 0;
+    this.status = trip && trip.status ? trip.status : 0;
   }
-  /**
-   * @param {object} trip object
-   */
-  createTrip(data) {
-    const newTrip = {
-      id: uuid.v4(),
-      bus_id: data.bus_id,
-      origin: data.origin,
-      destination: data.destination,
-      trip_date: data.trip_date,
-      fare: data.fare,
-      status: data.status,
-    };
-    this.trips.push(newTrip);
-    return newTrip;
+
+  async save() {
+    const params = [this.origin, this.destination, this.trip_date, this.fare, this.status];
+    try {
+      const { rows } = await db.query(`INSERT INTO trips 
+      (origin, destination, trip_date, fare, status)
+      VALUES ($1, $2, $3, $4, $5) RETURNING *`, params);
+      const newTrip = new Trip(rows[0]);
+      return newTrip;
+    } catch (error) {
+      throw error;
+    }
   }
-  /**
-   * @param {uuid} id
-   * @param {object} trip object
-   */
-  getTrip(id) {
-    return this.trips.find(trip => trip.id === id); 
-  }
-  /**
-   * @param {object} return all trips
-   */
-  getTrips() {
-    return this.trips;
-  }
-  /**
-   * @param {uuid} id
-   * @param {object} data
-   */
-  updateTrip(id, data) {
-    this.trips.forEach((trip, index) => {
-      if (trip.id === id) {
-        this.trips[index] = {
-          id: trip.id,
-          bus_id: data.bus_id || trip.bus_id,
-          origin: data.origin || trip.origin,
-          destination: data.destination || trip.destination,
-          trip_date: data.trip_date || trip.trip_date,
-          fare: data.fare || trip.fare,
-          status: data.status || trip.status,
-        };
-      }
-    });
-    return this.trips.find(trip => trip.id === id);
-  }
-  /**
-   * @param id
-   */
-  deleteTrip(id) {
-    this.trips.forEach((trip) => {
-      if (trip.id === id) {
-        this.trips.splice(id, 1);
-      }
-      return {};
-    });
+
+  static async findById(tripId) {
+    try {
+      const { rows } = await db.query('SELECT * FROM trips WHERE id=$1 LIMIT 1', [tripId]);
+      return rows.length ? new Trip(rows[0]) : false;
+    } catch (error) {
+      throw error;
+    }
   }
 }
-
-export default new Trip();

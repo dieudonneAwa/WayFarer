6fa4b87411abb2fe6d9249ef0dd1ff70198936bd
awadieudone <awadieudone@gmail.com>
2019-07-09 21:13:25 +0100

    [chore] : test database : set up a test database

--- db/index.js
+++ db/index.js
@@ -1,6 +1,13 @@
 import { Pool } from 'pg';
 
-const connectionString = 'postgresql://postgres:awa@localhost:5432/wayfarer';
+let connectionString = '';
+
+if (process.env.NODE_ENV === 'test') {
+  connectionString = 'postgresql://postgres:awa@localhost:5432/wayfarer_test';
+} else {
+  connectionString = 'postgresql://postgres:awa@localhost:5432/wayfarer';
+}
+
 
 const pool = new Pool({
   connectionString,

3bff2f8a43a5cf2d6cb0335f9b50e09d6e86bebf
awadieudone <awadieudone@gmail.com>
2019-07-10 21:17:41 +0100

    [chore] : routes : modify route endpoints

--- api/v1/routes/index.js
+++ api/v1/routes/index.js
@@ -10,28 +10,28 @@ const router = express.Router();
 router.get('/', (req, res) => res.send({ message: 'Welcome to my API' }));
 
 // authentications routes
-router.post('/api/v1/auth/signup', Signup.signUp);
-router.post('/api/v1/auth/signin', Signin.login);
+router.post('/auth/signup', Signup.signUp);
+router.post('/auth/signin', Signin.login);
 
 // trips routes
-router.post('/api/v1/trips', Trip.createTrip);
-router.get('/api/v1/trips', Trip.getAllTrips);
-router.get('/api/v1/trips/:tripId', Trip.getOneTrip);
-router.patch('/api/v1/trips/:tripId', Trip.updateTrip);
-router.delete('/api/v1/trips/:tripId', Trip.deleteTrip);
+router.post('/trips', Trip.createTrip);
+router.get('/trips', Trip.getAllTrips);
+router.get('/trips/:tripId', Trip.getOneTrip);
+router.patch('/trips/:tripId', Trip.updateTrip);
+router.delete('/trips/:tripId', Trip.deleteTrip);
 
 // bookings routes
-router.post('/api/v1/bookings', Booking.createBooking);
-router.get('/api/v1/bookings', Booking.getAllBookings);
-router.get('/api/v1/bookings/:bookingId', Booking.getOneBooking);
-router.patch('/api/v1/bookings/:bookingId', Booking.updateBooking);
-router.delete('/api/v1/bookings/:bookingId', Booking.deleteBooking);
+router.post('/bookings', Booking.createBooking);
+router.get('/bookings', Booking.getAllBookings);
+router.get('/bookings/:bookingId', Booking.getOneBooking);
+router.patch('/bookings/:bookingId', Booking.updateBooking);
+router.delete('/bookings/:bookingId', Booking.deleteBooking);
 
 // buses routes
-router.post('/api/v1/buses', Bus.addBus);
-router.get('/api/v1/buses', Bus.getAllBuses);
-router.get('/api/v1/buses/:busId', Bus.getOneBus);
-router.patch('/api/v1/buses/:busId', Bus.updatebus);
-router.delete('/api/v1/buses/:busId', Bus.deleteBus);
+router.post('/buses', Bus.addBus);
+router.get('/buses', Bus.getAllBuses);
+router.get('/buses/:busId', Bus.getOneBus);
+router.patch('/buses/:busId', Bus.updatebus);
+router.delete('/buses/:busId', Bus.deleteBus);
 
 export default router;

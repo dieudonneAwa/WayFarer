7c92a3f2a90506fc5e52729b8ce06e5fca65b188
awadieudone <awadieudone@gmail.com>
2019-07-11 21:56:40 +0100

    [chore] : migration : setup migrations for the api

---
+++ api/v1/db/migrations/index.js
@@ -0,0 +1,41 @@
+import { Client } from 'pg';
+import users from './users';
+import trips from './trips';
+import buses from './buses';
+import bookings from './bookings';
+
+let connectionString = '';
+
+if (process.env.NODE_ENV === 'test') {
+  connectionString = 'postgresql://postgres:awa@localhost:5432/wayfarer_test';
+} else {
+  connectionString = 'postgresql://postgres:awa@localhost:5432/wayfarer';
+}
+
+const client = new Client({
+  connectionString,
+});
+client.connect();
+
+const initTables = async () => {
+  try {
+    // DROP TABLES
+    await client.query(bookings.dropBookingsTable());
+    await client.query(trips.dropTripsTable());
+    await client.query(buses.dropBusesTable());
+    await client.query(users.dropUsersTable());
+
+    // CREATE TABLES
+    await client.query(users.createUsersTable());
+    await client.query(buses.createBusesTable());
+    await client.query(trips.createTripsTable());
+    await client.query(bookings.createBookingsTable());
+
+    await client.end();
+    console.log('Tables created successfully');
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+initTables();
